22a85f394e433dda1eb9ca90001a86132c7b9e07
Sarala Ravindra <35717051+sravindr1@users.noreply.github.com>
2018-05-21 02:13:07 -0400

    Update read_video_frames.m

--- read_video_frames.m
+++ read_video_frames.m
@@ -1,5 +1,6 @@
 clear all;
 tic;
+% Create video reader object 
 vidObj = VideoReader('test_video.avi');
 vidHeight = vidObj.Height;
 vidWidth = vidObj.Width;
@@ -7,6 +8,7 @@ vidWidth = vidObj.Width;
 s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
     'colormap',[]);
 
+% read frames
 k = 1;
 while hasFrame(vidObj)
     s(k).cdata = readFrame(vidObj);
